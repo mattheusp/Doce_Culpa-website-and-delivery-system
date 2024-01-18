5967116d37ec61561680906308fb491309e3e266
Mattheus Pereira <mattheusp382@gmail.com>
2024-01-18 15:00:57 -0300

    git add all

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# DoceCulpa-website-and-delivery-system
- website with ordering system included to place personalized orders via WhatsApp.
+# Sistema de Delivery via WhatsApp
+
+Projeto criado para **Doce Culpa** - um site bolos e tortas com sistema personalizado via WhatsApp!
+
+## Visão Geral
+
+A **Doce Culpa** é um site desenvolvido que permite aos usuários fazerem pedidos personalizados de bolos, tortas e doces, e serem direcionados para o WhatsApp da empresa com uma lista de pedidos escolhidos.
+
+## Fotos do projeto
+
+   - resultado do projeto Vercel: 
+
+![Inicio](img/screenshots/Screenshot 1.png)
+![inicio](img/screenshots/Screenshot 2.png)
+![serviços](img/screenshots/Screenshot 3.png)
+![menu](img/screenshots/Screenshot 4.png)
+![menu](img/screenshots/Screenshot 5.png)
+![testemunials](img/screenshots/Screenshot 6.png)
+![scheduling](img/screenshots/Screenshot 7.png)
+![footer](img/screenshots/Screenshot 8.png)
+
+
+
+## Ferramentas utilizadas: 
+   - Html
+   - Css
+   - JavaScript
+
+
+## Como Funciona
+
+1. ### funcionalidades
+   - escolher quantidade de produtos
+   - adicionar produtos ao carrinho
+   - adicionar endereço de entrega (codigo posta, bairro, numero)
+   - enviar os produtos escolhidos para o whatsapp da empresa
+   - taxa de entrega
+
+
+## Contato ✉️
+
+- E-mail: mattheusp382@gmail.com
+- LinkedIn: [Mattheus-Pereira](https://www.linkedin.com/in/mattheuspereira/)
+- Portfólio: [mtp-dev.com](https://mtpdev.com.br/)
+
+## Contribuição 🤝
+
+Se você deseja contribuir com algum projeto ou encontrar algum problema, fique à vontade para abrir uma nova issue ou enviar um pull request. Toda contribuição é bem-vinda!
+
+## Licença ⚖️
+
+Este portfolio é licenciado sob a [Licença MIT](https://opensource.org/licenses/MIT).
---
+++ js/dados.js
@@ -0,0 +1,134 @@
+var MENU = {
+    "bolos": [
+        {
+            "id": "bolo-de-morango",
+            "img": "./img/cardapio/bolos/bolo 1.png",
+            "name": "bolo de morango",
+            "dsc": "bolo de morango",
+            "price": 99
+        },
+        {
+            "id": "bolo-perola-negra",
+            "img": "img/cardapio/bolos/bolo 2.png",
+            "name": "Bolo perola negra",
+            "dsc": "Bolo perola negra",
+            "price": 49
+        },
+        {
+            "id": "ouro-branco",
+            "img": "img/cardapio/bolos/bolo 3.png",
+            "name": "ouro branco",
+            "dsc": "bolo de ouro branco",
+            "price": 99
+        },
+        {
+            "id": "red-velvet",
+            "img": "img/cardapio/bolos/bolo 4.png",
+            "name": "red Velvet",
+            "dsc": "red Velvet",
+            "price": 99
+        },
+        {
+            "id": "red-velvelt",
+            "img": "./img/cardapio/bolos/bolo 5.png",
+            "name": "red Velvelt",
+            "dsc": "red Velvelt",
+            "price": 89
+        },
+        {
+            "id": "frutas-vermelhas",
+            "img": "./img/cardapio/bolos/bolo 6.png",
+            "name": "frutas vermelhas",
+            "dsc": "frutas vermelhas",
+            "price": 90.00
+        },
+        {
+            "id": "bolo-de-limao",
+            "img": "./img/cardapio/bolos/bolo 7.png",
+            "name": "bolo de limão",
+            "dsc": "bolo de limão",
+            "price": 85.00
+        },
+        {
+            "id": "bolo-de-morango",
+            "img": "./img/cardapio/bolos/bolo  8.png",
+            "name": "bolo de morango",
+            "dsc": "bolo de morango",
+            "price": 90.00
+        }
+    ],
+    "sobremesas": [
+        {
+            "id": "3x-mini-redvelvet-2x-mini-doce-de-leite-3x-mini-prestigio-2x-mini-brigadeiro",
+            "img": "./img/cardapio/sobremesas/Kit 1.png",
+            "name": "3x mini redvelvet 2x mini doce de leite 3x mini prestigio 2x mini brigadeiro",
+            "dsc": "3x mini redvelvet 2x mini doce de leite 3x mini prestigio 2x mini brigadeiro",
+            "price": 39.99
+        },
+        {
+            "id": "9x-mini-chocolate-crocante",
+            "img": "./img/cardapio/sobremesas/Kit 2.png",
+            "name": "9x mini chocolate crocante",
+            "dsc": "3x mini redvelvet 2x mini doce de leite 3x mini prestigio 2x mini brigadeiro",
+            "price": 39.95
+        },
+    ],
+    "doces": [
+        {
+            "id": "brownies-diversos",
+            "img": "./img/cardapio/doces/doce 1.png",
+            "name": "Brownies diversos",
+            "dsc": "Brownies diversos",
+            "price": 21.00
+        },
+        {
+            "id": "brownie-de-chocolate",
+            "img": "./img/cardapio/doces/Doce 2.png",
+            "name": "Brownie de chocolate",
+            "dsc": "Brownie de chocolate",
+            "price": 12.99
+        },
+        {
+            "id": "bolo-de-cenoura",
+            "img": "./img/cardapio/doces/Doce 3.png",
+            "name": "Bolo de cenoura",
+            "dsc": "Bolo de cenoura",
+            "price": 8.00
+        },
+        {
+            "id": "queijo-do-reino",
+            "img": "./img/cardapio/doces/Doce 4.png",
+            "name": "queijo do reino",
+            "dsc": "bolo de Queijo do reino",
+            "price": 6.00
+        },
+        {
+            "id": "brownie-de-chocolate-com-cobertura-de-chocolate-branco",
+            "img": "./img/cardapio/doces/Doce 5.png",
+            "name": "brownie de chocolate com cobertura de chocolate branco",
+            "dsc": "brownie de chocolate com cobertura de chocolate branco",
+            "price": 6.00
+        },
+        {
+            "id": "bolo-simples",
+            "img": "./img/cardapio/doces/Doce 6.png",
+            "name": "bolo simples",
+            "dsc": "bolo simples",
+            "price": 7.00
+        },
+        {
+            "id": "red-velvet",
+            "img": "./img/cardapio/doces/Doce 7.png",
+            "name": "red Velvet",
+            "dsc": "red Velvet",
+            "price": 6.00
+        },
+        {
+            "id": "red-velvet-inverso",
+            "img": "./img/cardapio/doces/Doce 8.png",
+            "name": "red Velvet inverso",
+            "dsc": "red Velvet inverso",
+            "price": 5.50
+        },
+    ],
+}
